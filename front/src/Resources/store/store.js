58bf5efd0d411f80d017f85d0897c613aaa77b11
Beka Dugladze <125121125+BekaDugladze@users.noreply.github.com>
2023-10-13 14:22:49 +0400

    Delete front directory

--- front/src/Resources/store/store.js
+++
@@ -1,7 +0,0 @@
-// store.js
-import { createStore } from 'redux';
-import rootReducer from '../reducer/reducer'; // Assuming you have a reducer file
-
-const store = createStore(rootReducer);
-
-export default store;
